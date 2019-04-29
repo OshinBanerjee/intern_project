015b908936ab001a2adf64d4cb8341044be02972
OshinBanerjee <46805679+OshinBanerjee@users.noreply.github.com>
2019-04-29 17:12:58 +0530

    Update appsettings.ts

--- dashboard/web/src/app/appsettings.ts
+++ dashboard/web/src/app/appsettings.ts
@@ -1,4 +1,5 @@
 export class Appsettings {
     public static LOCAL_ENDPOINT='http://localhost:9004/dashboard/';
     public static ADDBATCH='addBatch';
+  public static ADDSCHEDULE='addSchedule';
 }
